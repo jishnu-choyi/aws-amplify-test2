3fe74bfc3363ada3d68a9c7f71b894b7b56bd239
Jishnu Choyi <c.jishnu@gmail.com>
2023-08-17 21:49:09 +0530

    add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsamplifytest2": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
